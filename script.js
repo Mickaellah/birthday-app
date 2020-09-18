d4b219d7181e1729236daa187f32e7fe9d49b143
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-09-18 09:20:55 +0300

    Used popup instead of using modal in the add item button to give a form

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const personList = document.querySelector('.people');
-const outerModal = document.querySelector('.outer_modal');
-const innerModal = document.querySelector('.inner_modal');
 const addBttn = document.querySelector('.add');
 
 
 async function fetchPeople() {
     const response = await fetch('./people.json');
     const data = await response.json();
-    return data;
+    let result = data;
+    return result;
 }
 
 
@@ -42,53 +41,15 @@ async function displayPeople() {
 displayPeople();
 
 
-// const handleClick = (e) => {
-//     const updateItem = e.target.closest('button.edit');
-//     if (updateItem) {
-//         const id = Number(updateItem.id);
-//         editItem(id);
-//     }
-// }
+const wait = (ms = 0) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// const editItem = (id) => {
-//     const itemToEdit = people.find(person => person.id === id);
 
-//     const html = `
-//         <div>
-//             <fieldset>
-//                 <label for="first_name">First name</label>
-//                 <input type="text" id="first_name" name="firstname">
-//             </fieldset>
-//             <fieldset>
-//                 <label for="last_name">Last name</label>
-//                 <input type="text" id="last_name" name="lastname">
-//             </fieldset>
-//             <fieldset>
-//                 <label for="birthday">Birthday</label>
-//                 <input type="number" id="birthday" name="birthday">
-//             </fieldset>
-//             <fieldset>
-//                 <label for="profile">Avatar image</label>
-//                 <input type="url" id="profile" name="picture">
-//             </fieldset>
-//             <fieldset>
-//                 <label for=""></label>
-//                 <input type="text" id="">
-//             </fieldset>
-//         </div>
-//     `;
-//     innerModal.innerHTML = html;
-// };
 
-// personList.addEventListener('click', handleClick);
-
-// const deleteItem = (id) => {
-
-// };
-
-const handleAddBttn = (e) => {
-    return new Promise(function(resolve) {
-        const form = document.createElement('form');
+const handleAddBttn = async (e) => {
+    return new Promise(async function(resolve, reject) {
+        let form = document.createElement('form');
         form.classList.add('form');
 
         const html = `
@@ -96,63 +57,82 @@ const handleAddBttn = (e) => {
             <h2>Add your name and your birthday</h2>
             <fieldset>
                 <label for="first_name">First name</label>
-                <input type="text" id="first_name" name="firstname" placeholder="enter your firstname">
+                <input type="text" id="first_name" name="firstname" placeholder="enter your firstname" required>
             </fieldset>
             <fieldset>
                 <label for="last_name">Last name</label>
-                <input type="text" id="last_name" name="lastname" placeholder="enter your lastname">
+                <input type="text" id="last_name" name="lastname" placeholder="enter your lastname" required>
             </fieldset>
             <fieldset>
                 <label for="birthday">Birthday</label>
-                <input type="number" id="birthday" name="birthday" placeholder="enter your birthday">
+                <input type="date" id="birthday" name="birthday" placeholder="enter your birthday" required>
             </fieldset>
             <fieldset>
                 <label for="profile">Avatar image</label>
                 <input type="url" id="profile" name="picture" placeholder="https://onja.org/wp-content/uploads/2019/08/Clopedia@2x-430x520.jpg">
             </fieldset>
             <button class="submitbttn" type="submit">Submit</button>
+
         </div>
         `;
         form.innerHTML = html;
 
-        innerModal.innerHTML = html;
-        outerModal.classList.add('open');
         resolve();
 
+        const destroyPopup = async () => {
+            form.classList.remove('open');
+            await wait(1000);
+            form.remove();
+            form = null;
+        }
+        
 
-        form.addEventListener('submit', handleSubmit);
-        e.preventDefault();
-        const formEl = e.currentTarget;
+        if (reject) {
+            const skipButton = document.createElement('button');
+            skipButton.type = "button";
+            skipButton.textContent = "Cancel"
+            skipButton.classList.add('cancel');
 
-        const newDate = {
-            firstname: formEl.firstname.value,
-            lastname: formEl.lastname.value,
-            birthday: formEl.birthday.value,
-            picture: formEl.picture.value,
-            id: Date.now(),
-        };
+            form.firstElementChild.appendChild(skipButton);
+            skipButton.addEventListener('click', () => {
+                resolve(null);
+                destroyPopup(form);
+            }, { once: true });
+        }
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            resolve(e.target.input.value);
+            destroyPopup(form);
+
+            const formEl = e.currentTarget;
+
+            const newDate = {
+                firstname: formEl.firstname.value,
+                lastname: formEl.lastname.value,
+                birthday: formEl.birthday.value,
+                picture: formEl.picture.value,
+                id: Date.now(),
+            };
+            result.push(newDate);
+            displayPeople();
+            destroyPopup(form);
+            form.reset();
+
+        }, { once: true });
+
+
+
+        
+
+        document.body.appendChild(form);
+        await wait(50);
+        form.classList.add('open');
     });
 };
 
 
-const closeModal = () => {
-    outerModal.classList.remove('open');
-}
-
-
 addBttn.addEventListener('click', handleAddBttn);
-outerModal.addEventListener('click', (e) => {
-    const isOutside = !e.target.closest('.inner_modal');
-    if (isOutside) {
-        closeModal();
-    }
-});
-window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") {
-        closeModal();
-    }
-});
-
 
 
 
